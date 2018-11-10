95b2092aa261b5d6c58d77e7e7602b1df0ace894
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-11-11 00:22:24 +0530

    "ads2_m5_ba1_1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M5/Bonus-1/Solution.java
+++ ASSIGNMENTS/M5/Bonus-1/Solution.java
@@ -14,38 +14,35 @@ public class Solution {
         String[] tokens = scan.nextLine().split(" ");
         int vertices = Integer.parseInt(tokens[0]);
         int edges = Integer.parseInt(tokens[1]);
-        Graph g = new Graph(vertices + 1);
         if (edges == 0) {
             System.out.println(edges);
         } else {
-            while (edges > 0) {
-                String[] connections = scan.nextLine().split(" ");
-                int a = Integer.parseInt(connections[0]);
-                int b = Integer.parseInt(connections[1]);
-                g.addEdge(a, b);
-                edges--;
+            Graph g = new Graph(vertices + 1);
+            while (scan.hasNext()) {
+                String[] tokens1 = scan.nextLine().split(" ");
+                g.addEdge(Integer.parseInt(tokens1[0]),
+                            Integer.parseInt(tokens1[1]));
             }
-        }
-        CC connected = new CC(g);
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int[] array = connected.idArray();
-        for (int i = 0; i < g.V(); i++) {
-            if (g.hasParallelEdges(i)) {
-                count1++;
-            }
-            int count4 = 0;
-            count3 = array[i];
-            for (int j = 0; j < array.length; j++) {
-                if (count3 == array[j]) {
-                    count4++;
+            CC connected = new CC(g);
+            int count1 = 0;
+            int count2 = 0;
+            int[] array = connected.idArray();
+            for (int i = 0; i < g.V(); i++) {
+                if (g.hasParallelEdges(i)) {
+                    count1++;
+                }
+                int idcount = 0;
+                int id = array[i];
+                for (int j = 0; j < array.length; j++) {
+                    if (id == array[j]) {
+                        idcount++;
+                    }
+                }
+                if (count2 < idcount) {
+                    count2 = idcount;
                 }
             }
-            if (count2 < count4) {
-                count2 = count4;
-            }
+            System.out.println(count1 + count2);
         }
-        System.out.println(count1 + count2);
     }
 }
