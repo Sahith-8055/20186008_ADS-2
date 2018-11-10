19984a345d40a58ab92828dc90996f5409ba2490
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-11-10 23:42:30 +0530

    "ads2_m5_ba1_1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ASSIGNMENTS/M5/Bonus-1/Graph.java
@@ -0,0 +1,95 @@
+public class Graph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    /**
+     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param  V number of vertices
+     */
+    Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int E() {
+        return E;
+    }
+
+
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param  v one vertex in the edge
+     * @param  w the other vertex in the edge
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
+    public void addEdge(int v, int w) {
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+
+    /**
+     * Returns the vertices adjacent to vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(int v) {
+        return adj[v].size();
+    }
+
+
+    /**
+     * Returns a string representation of this graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + "\n");
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+}
---
+++ ASSIGNMENTS/M5/Bonus-1/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // Unused Constructor.
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] tokens = scan.nextLine().split(" ");
+        int vertices = Integer.parseInt(tokens[0]);
+        int edges = Integer.parseInt(tokens[1]);
+        Graph g = new Graph(vertices);
+        while (edges > 0) {
+            String[] connections = scan.nextLine().split(" ");
+            int a = Integer.parseInt(connections[0]);
+            int b = Integer.parseInt(connections[1]);
+            g.addEdge(a, b);
+            edges--;
+        }
+        CC connected = new CC(g);
+        int m = connected.count();
+        Queue<Integer>[] components = (Queue<Integer>[])new Queue[m];
+        for (int i = 0; i < m; i++) {
+            components[i] = new Queue<Integer>();
+        }
+        for (int j = 0; j < g.V(); j++) {
+            components[connected.id(j)].enqueue(j);
+        }
+        int count = 0;
+        for (int k = 0; k < g.V(); k++) {
+            if (count <= components[k].size()) {
+                count = components[k].size();
+            }
+        }
+        System.out.println(count - 1);
+    }
+}
