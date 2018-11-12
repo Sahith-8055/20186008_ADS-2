402443ad4c7f8a705f5d7976a0ab651cb4b0f126
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-11-12 12:33:30 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M13/Radix Sorts/Solution.java
+++ ASSIGNMENTS/M13/Radix Sorts/Solution.java
@@ -174,7 +174,7 @@ class MSD {
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 
     /**
      * Constructs the object.
@@ -182,6 +182,11 @@ public class Solution {
     private Solution() {
         //Unused Constructor.
     }
+    /**
+     * {Client Program}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int numberofLines = Integer.parseInt(scan.nextLine());
