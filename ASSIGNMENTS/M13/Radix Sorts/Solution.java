06284b4167e69546f0479bc62c8e3227f3fcdc01
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-11-12 12:14:56 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M13/Radix Sorts/Solution.java
+++ ASSIGNMENTS/M13/Radix Sorts/Solution.java
@@ -1,100 +1,173 @@
 import java.util.Scanner;
-class LSD {
-    private static final int BITS_PER_BYTE = 8;
 
-    public LSD() {
+class MSD {
+    private static final int BITS_PER_BYTE =   8;
+    private static final int BITS_PER_INT  =  32;   // each Java int is 32 bits
+    private static final int R             = 256;   // extended ASCII alphabet size
+    private static final int CUTOFF        =  15;   // cutoff to insertion sort
+
+    public MSD() {
         // do not instantiate.
     }
 
-    /**
-      * Rearranges the array of W-character strings in ascending order.
-      *
-      * @param a the array to be sorted
-      * @param w the number of characters per string
-      */
-    public void sort(final String[] a, final int w) {
+   /**
+     * Rearranges the array of extended ASCII strings in ascending order.
+     *
+     * @param a the array to be sorted
+     */
+    public static void sort(String[] a) {
         int n = a.length;
-        int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
-
-        for (int d = w - 1; d >= 0; d--) {
-            // sort by key-indexed counting on dth character
-
-            // compute frequency counts
-            int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++) {
-                count[a[i].charAt(d) + 1]++;
-            }
-
-            // compute cumulates
-            for (int r = 0; r < R; r++) {
-                count[r + 1] += count[r];
-            }
-
-            // move data
-            for (int i = 0; i < n; i++) {
-                aux[count[a[i].charAt(d)]++] = a[i];
-            }
-
-            // copy back
-            for (int i = 0; i < n; i++) {
-                a[i] = aux[i];
-            }
-        }
+        sort(a, 0, n-1, 0, aux);
     }
 
-    /**
-      * Rearranges the array of 32-bit integers in ascending order.
-      * This is about 2-3x faster than Arrays.sort().
-      *
-      * @param a the array to be sorted
-      */
-    public void sort(final int[] a) {
-        final int BITS = 32;                 // each int is 32 bits
-        final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
-        final int MASK = R - 1;              // 0xFF
-        final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
+    // return dth character of s, -1 if d = length of string
+    private static int charAt(String s, int d) {
+        assert d >= 0 && d <= s.length();
+        if (d == s.length()) return -1;
+        return s.charAt(d);
+    }
 
+    // sort from a[lo] to a[hi], starting at the dth character
+    private static void sort(String[] a, int lo, int hi, int d, String[] aux) {
+
+        // cutoff to insertion sort for small subarrays
+        if (hi <= lo + CUTOFF) {
+            insertion(a, lo, hi, d);
+            return;
+        }
+
+        // compute frequency counts
+        int[] count = new int[R+2];
+        for (int i = lo; i <= hi; i++) {
+            int c = charAt(a[i], d);
+            count[c+2]++;
+        }
+
+        // transform counts to indicies
+        for (int r = 0; r < R+1; r++)
+            count[r+1] += count[r];
+
+        // distribute
+        for (int i = lo; i <= hi; i++) {
+            int c = charAt(a[i], d);
+            aux[count[c+1]++] = a[i];
+        }
+
+        // copy back
+        for (int i = lo; i <= hi; i++)
+            a[i] = aux[i - lo];
+
+
+        // recursively sort for each character (excludes sentinel -1)
+        for (int r = 0; r < R; r++)
+            sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+    }
+
+
+    // insertion sort a[lo..hi], starting at dth character
+    private static void insertion(String[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
+                exch(a, j, j-1);
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // is v less than w, starting at character d
+    private static boolean less(String v, String w, int d) {
+        // assert v.substring(0, d).equals(w.substring(0, d));
+        for (int i = d; i < Math.min(v.length(), w.length()); i++) {
+            if (v.charAt(i) < w.charAt(i)) return true;
+            if (v.charAt(i) > w.charAt(i)) return false;
+        }
+        return v.length() < w.length();
+    }
+
+
+   /**
+     * Rearranges the array of 32-bit integers in ascending order.
+     * Currently assumes that the integers are nonnegative.
+     *
+     * @param a the array to be sorted
+     */
+    public static void sort(int[] a) {
         int n = a.length;
         int[] aux = new int[n];
+        sort(a, 0, n-1, 0, aux);
+    }
 
-        for (int d = 0; d < w; d++) {
+    // MSD sort from a[lo] to a[hi], starting at the dth byte
+    private static void sort(int[] a, int lo, int hi, int d, int[] aux) {
 
-            // compute frequency counts
-            int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
-                count[c + 1]++;
-            }
-
-            // compute cumulates
-            for (int r = 0; r < R; r++) {
-                count[r + 1] += count[r];
-            }
-
-            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
-            if (d == w - 1) {
-                int shift1 = count[R] - count[R / 2];
-                int shift2 = count[R / 2];
-                for (int r = 0; r < R / 2; r++) {
-                    count[r] += shift1;
-                }
-                for (int r = R / 2; r < R; r++) {
-                    count[r] -= shift2;
-                }
-            }
-
-            // move data
-            for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
-                aux[count[c]++] = a[i];
-            }
-
-            // copy back
-            for (int i = 0; i < n; i++) {
-                a[i] = aux[i];
-            }
+        // cutoff to insertion sort for small subarrays
+        if (hi <= lo + CUTOFF) {
+            insertion(a, lo, hi, d);
+            return;
         }
+
+        // compute frequency counts (need R = 256)
+        int[] count = new int[R+1];
+        int mask = R - 1;   // 0xFF;
+        int shift = BITS_PER_INT - BITS_PER_BYTE*d - BITS_PER_BYTE;
+        for (int i = lo; i <= hi; i++) {
+            int c = (a[i] >> shift) & mask;
+            count[c + 1]++;
+        }
+
+        // transform counts to indicies
+        for (int r = 0; r < R; r++)
+            count[r+1] += count[r];
+
+/************* BUGGGY CODE.
+        // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+        if (d == 0) {
+            int shift1 = count[R] - count[R/2];
+            int shift2 = count[R/2];
+            for (int r = 0; r < R/2; r++)
+                count[r] += shift1;
+            for (int r = R/2; r < R; r++)
+                count[r] -= shift2;
+        }
+************************************/
+        // distribute
+        for (int i = lo; i <= hi; i++) {
+            int c = (a[i] >> shift) & mask;
+            aux[count[c]++] = a[i];
+        }
+
+        // copy back
+        for (int i = lo; i <= hi; i++)
+            a[i] = aux[i - lo];
+
+        // no more bits
+        if (d == 4) return;
+
+        // recursively sort for each character
+        if (count[0] > 0)
+            sort(a, lo, lo + count[0] - 1, d+1, aux);
+        for (int r = 0; r < R; r++)
+            if (count[r+1] > count[r])
+                sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+    }
+
+    // TODO: insertion sort a[lo..hi], starting at dth character
+    private static void insertion(int[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && a[j] < a[j-1]; j--)
+                exch(a, j, j-1);
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
     public void toString(final String[] a) {
         StringBuilder sb = new StringBuilder();
@@ -107,7 +180,6 @@ class LSD {
         System.out.println(sb.toString());
     }
 }
-
 /**
  * Class for solution.
  */
@@ -126,8 +198,8 @@ public class Solution {
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextLine();
         }
-        LSD l = new LSD();
-        l.sort(array, 8);
+        MSD l = new MSD();
+        l.sort(array);
         l.toString(array);
     }
 }
