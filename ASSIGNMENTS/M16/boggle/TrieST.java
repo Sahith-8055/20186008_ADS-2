290a00e40673c4d430dac8b0a7219d36c76616d2
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-11-16 12:22:03 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M16/boggle/TrieST.java
+++ ASSIGNMENTS/M16/boggle/TrieST.java
@@ -1,34 +1,60 @@
+/**
+ * Class for trie st.
+ *
+ * @param      <Value>  The value
+ */
 public class TrieST<Value> {
-    private static final int R = 26;        // extended ASCII
+    /**
+     * {extended ASCII}.
+     */
+    private static final int R = 26;
+    /**
+     * {root of trie}.
+     */
+    private Node root;
+    /**
+     * {number of keys in trie}.
+     */
+    private int n;
 
-
-    private Node root;      // root of trie
-    private int n;          // number of keys in trie
-
-    // R-way trie node
+    /**
+     * Class for node.
+     */
     private static class Node {
+        /**
+         * {Value}.
+         */
         private Object val;
+        /**
+         * {next of type node}.
+         */
         private Node[] next = new Node[R];
     }
 
-   /**
-     * Initializes an empty string symbol table.
-     */
+    /**
+      * Initializes an empty string symbol table.
+      */
     public TrieST() {
+        //Unused Constructor.
     }
 
 
     /**
      * Returns the value associated with the given key.
      * @param key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
+     * @return the value associated with the given key
+     * and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(String key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+    public Value get(final String key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to get() is null");
+        }
         Node x = get(root, key, 0);
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         return (Value) x.val;
     }
 
@@ -36,22 +62,45 @@ public class TrieST<Value> {
      * Does this symbol table contain the given key?
      * @param key the key
      * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
+     * {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public boolean contains(String key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+    public boolean contains(final String key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to contains() is null");
+        }
         return get(key) != null;
     }
 
-    private Node get(Node x, String key, int d) {
-        if (x == null) return null;
-        if (d == key.length()) return x;
+    /**
+     * {Private method for get}.
+     *
+     * @param      x     {Node}
+     * @param      key   The key
+     * @param      d     {Integer}
+     *
+     * @return     {Node}
+     */
+    private Node get(final Node x, final String key, final int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
         char c = key.charAt(d);
-        return get(x.next[c - 65], key, d+1);
+        return get(x.next[c - 65], key, d + 1);
     }
 
-    public boolean hasPrefix(String str) {
+    /**
+     * Determines if it has prefix.
+     *
+     * @param      str   The string
+     *
+     * @return     True if has prefix, False otherwise.
+     */
+    public boolean hasPrefix(final String str) {
         Node node = get(root, str, 0);
         return node != null;
     }
@@ -64,133 +113,34 @@ public class TrieST<Value> {
      * @param val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(String key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
-        if (val == null) delete(key);
-        else root = put(root, key, val, 0);
+    public void put(final String key, final Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+        } else {
+            root = put(root, key, val, 0);
+        }
     }
 
-    private Node put(Node x, String key, Value val, int d) {
-        if (x == null) x = new Node();
+    private Node put(final Node x, final String key,
+                     final Value val, final int d) {
+        Node x1 = x;
+        if (x1 == null) {
+            x1 = new Node();
+        }
         if (d == key.length()) {
-            if (x.val == null) n++;
-            x.val = val;
-            return x;
+            if (x1.val == null) {
+                n++;
+            }
+            x1.val = val;
+            return x1;
         }
         char c = key.charAt(d);
-        x.next[c - 65] = put(x.next[c - 65], key, val, d+1);
-        return x;
-    }
-
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
-        return n;
-    }
-
-    /**
-     * Is this symbol table empty?
-     * @return {@code true} if this symbol table is empty and {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     * @return all keys in the symbol table as an {@code Iterable}
-     */
-    public Iterable<String> keys() {
-        return keysWithPrefix("");
-    }
-
-    /**
-     * Returns all of the keys in the set that start with {@code prefix}.
-     * @param prefix the prefix
-     * @return all of the keys in the set that start with {@code prefix},
-     *     as an iterable
-     */
-    public Iterable<String> keysWithPrefix(String prefix) {
-        Queue<String> results = new Queue<String>();
-        Node x = get(root, prefix, 0);
-        collect(x, new StringBuilder(prefix), results);
-        return results;
-    }
-
-    private void collect(Node x, StringBuilder prefix, Queue<String> results) {
-        if (x == null) return;
-        if (x.val != null) results.enqueue(prefix.toString());
-        for (char c = 0; c < R; c++) {
-            prefix.append(c);
-            collect(x.next[c], prefix, results);
-            prefix.deleteCharAt(prefix.length() - 1);
-        }
-    }
-
-    /**
-     * Returns all of the keys in the symbol table that match {@code pattern},
-     * where . symbol is treated as a wildcard character.
-     * @param pattern the pattern
-     * @return all of the keys in the symbol table that match {@code pattern},
-     *     as an iterable, where . is treated as a wildcard character.
-     */
-    public Iterable<String> keysThatMatch(String pattern) {
-        Queue<String> results = new Queue<String>();
-        collect(root, new StringBuilder(), pattern, results);
-        return results;
-    }
-
-    private void collect(Node x, StringBuilder prefix, String pattern, Queue<String> results) {
-        if (x == null) return;
-        int d = prefix.length();
-        if (d == pattern.length() && x.val != null)
-            results.enqueue(prefix.toString());
-        if (d == pattern.length())
-            return;
-        char c = pattern.charAt(d);
-        if (c == '.') {
-            for (char ch = 0; ch < R; ch++) {
-                prefix.append(ch);
-                collect(x.next[ch], prefix, pattern, results);
-                prefix.deleteCharAt(prefix.length() - 1);
-            }
-        }
-        else {
-            prefix.append(c);
-            collect(x.next[c], prefix, pattern, results);
-            prefix.deleteCharAt(prefix.length() - 1);
-        }
-    }
-
-    /**
-     * Returns the string in the symbol table that is the longest prefix of {@code query},
-     * or {@code null}, if no such string.
-     * @param query the query string
-     * @return the string in the symbol table that is the longest prefix of {@code query},
-     *     or {@code null} if no such string
-     * @throws IllegalArgumentException if {@code query} is {@code null}
-     */
-    public String longestPrefixOf(String query) {
-        if (query == null) throw new IllegalArgumentException("argument to longestPrefixOf() is null");
-        int length = longestPrefixOf(root, query, 0, -1);
-        if (length == -1) return null;
-        else return query.substring(0, length);
-    }
-
-    // returns the length of the longest string key in the subtrie
-    // rooted at x that is a prefix of the query string,
-    // assuming the first d character match and we have already
-    // found a prefix match of given length (-1 if no such match)
-    private int longestPrefixOf(Node x, String query, int d, int length) {
-        if (x == null) return length;
-        if (x.val != null) length = d;
-        if (d == query.length()) return length;
-        char c = query.charAt(d);
-        return longestPrefixOf(x.next[c], query, d+1, length);
+        x1.next[c - 65] = put(x1.next[c - 65], key, val, d + 1);
+        return x1;
     }
 
     /**
@@ -208,10 +158,9 @@ public class TrieST<Value> {
         if (d == key.length()) {
             if (x.val != null) n--;
             x.val = null;
-        }
-        else {
+        } else {
             char c = key.charAt(d);
-            x.next[c] = delete(x.next[c], key, d+1);
+            x.next[c] = delete(x.next[c], key, d + 1);
         }
 
         // remove subtrie rooted at x if it is completely empty
